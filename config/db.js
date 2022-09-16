c91ab328e139fb02b098026b3d365cd617f085f1
MatcFowler <fowlerj9@gmatc.matc.edu>
2022-09-16 16:15:44 -0600

    users model

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import  mongoose  from 'mongoose';
-=======
 import  Mongoose  from 'mongoose';
->>>>>>> ba842a41db7caf33f56380a90f6da86c46ce9b52
+
 import  config from 'config';
 
 const db = config.get('mongoURI');
@@ -10,11 +8,7 @@ const db = config.get('mongoURI');
 const connectDatabase = async () =>
 {
     try{
-<<<<<<< HEAD
-         await mongoose.connect(db,
-=======
          Mongoose.connect(db,
->>>>>>> ba842a41db7caf33f56380a90f6da86c46ce9b52
             { useUnifiedTopology: true });
         console.log('Connected to MongoDB');   
     }catch(error)
