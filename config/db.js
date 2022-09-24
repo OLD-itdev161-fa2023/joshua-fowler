a2f7c87bb6dc670d6c7670161e21e2d621f46096
MatcFowler <fowlerj9@gmatc.matc.edu>
2022-09-24 13:12:38 -0600

    completed activity 4

--- config/db.js
+++ config/db.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 import  mongoose  from 'mongoose';
-=======
-import  Mongoose  from 'mongoose';
->>>>>>> ba842a41db7caf33f56380a90f6da86c46ce9b52
 import  config from 'config';
-
 const db = config.get('mongoURI');
 
 const connectDatabase = async () =>
 {
     try{
-<<<<<<< HEAD
-         await mongoose.connect(db,
-=======
-         Mongoose.connect(db,
->>>>>>> ba842a41db7caf33f56380a90f6da86c46ce9b52
-            { useUnifiedTopology: true });
+await mongoose.connect(db,{ 
+            useUnifiedTopology: true 
+        });
         console.log('Connected to MongoDB');   
     }catch(error)
     {
--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
 const app = express();
 
-connectDatabase();  
+connectDatabase();
 
-app.get('/', (req, res)=>
+    app.use(express.json({ extended: false }));
+    app.use(
+        cors({
+            origin: 'http://localhost:3000'
+        })
+    );
+
+    app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
+    );
+
+    app.get('/api/', (req, res) => res.send('http get request sent to api'));
+
+    app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name')
+        .not()
+        .isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
 );
 
-
-app.listen(3000, () => console.log('express server running on port 3000'));
+// Connection listener
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
