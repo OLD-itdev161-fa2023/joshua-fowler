8ca73c9e0ddc1debd38f5a3cf38e4e3557df9fe4
MatcFowler <fowlerj9@gmatc.matc.edu>
2022-10-22 23:14:19 -0600

    Completed Activity-7

--- config/db.js
+++ config/db.js
@@ -1,21 +1,23 @@
-import  mongoose  from 'mongoose';
-import  Mongoose  from 'mongoose';
+import mongoose from 'mongoose';
+import config from 'config';
 
-import  config from 'config';
+// Get the connection string
 const db = config.get('mongoURI');
 
-const connectDatabase = async () =>
-{
-await mongoose.connect(db,{ useUnifiedTopology: true  });
-         Mongoose.connect(db,
-            { useUnifiedTopology: true });
-        console.log('Connected to MongoDB');   
-    }catch(error)
-    {
-        console.error(error.message);
+// Connect to MongoDB
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error.message);
 
-        process.exit(1);
-    }
+    // Exit with failure code
+    process.exit(1);
+  }
 };
 
 export default connectDatabase;
